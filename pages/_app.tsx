575c371335a56a176c65f973199ea9f2e3ef8892
jaypandya2k5+laptop <jaypandya2k5+laptop@gmail.com>
2021-04-15 15:08:33 -0400

    add tailwindcss and sass

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "../styles/globals.scss";
+import * as React from "react";
+
+const MyApp = ({ Component, pageProps }: any) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 const HomePage = () => {
-  return <div>Welcome to Next.js!</div>
-}
+  return <div>Welcome to Next.js!</div>;
+};
 
-export default HomePage
+export default HomePage;
